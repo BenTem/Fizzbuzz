2ffe43f7b31b59bf5845f8ae164348df9b09a4fc
Ben <benctemple@gmail.com>
2015-02-23 15:31:00 -0800

    added fizzbuzz

---
+++ fb.rb
@@ -0,0 +1,17 @@
+i = 0
+
+until i > 99
+
+    i = i + 1
+    
+    if (i % 3 == 0) && (i % 5 == 0)
+        puts "FizzBuzz"
+    elsif i % 5 == 0
+        puts "Buzz"
+    elsif  i % 3 == 0
+        puts "Fizz"
+    else
+        puts i
+    end
+    
+end
